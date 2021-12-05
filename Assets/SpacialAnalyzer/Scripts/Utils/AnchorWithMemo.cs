162e61ccc0ef59bef27d2b74bdf4c06715989c35
nekomimi-daimao <35223467+nekomimi-daimao@users.noreply.github.com>
2021-12-06 00:58:27 +0900

    add anchorWithMemo

---
+++ Assets/SpacialAnalyzer/Scripts/Utils/AnchorWithMemo.cs
@@ -0,0 +1,55 @@
+using UniRx;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SpacialAnalyzer.Scripts.Utils
+{
+    public sealed class AnchorWithMemo : MonoBehaviour
+    {
+        [SerializeField]
+        private Text _text = null;
+
+        public string Memo
+        {
+            get => _text.text;
+            set => _text.text = value;
+        }
+
+        public Transform SelfTs { get; private set; }
+        public Vector3 Position => SelfTs.position;
+        public Quaternion Rotation => SelfTs.rotation;
+
+        private Transform _cameraTs = null;
+
+        private void OnEnable()
+        {
+            SelfTs = this.transform;
+            _cameraTs = Camera.main?.transform;
+
+            if (_text == null || _cameraTs == null)
+            {
+                return;
+            }
+
+            var textParentTs = _text.transform.parent;
+
+            Observable.EveryUpdate()
+                .TakeUntilDisable(gameObject)
+                .Subscribe(_ =>
+                {
+                    var look = _cameraTs.position - Position;
+                    if (look != Vector3.zero)
+                    {
+                        textParentTs.rotation = Quaternion.LookRotation(-look, Vector3.up);
+                    }
+                });
+        }
+
+        public AnchorWithMemo Create(string memo, Vector3 pos, Quaternion rot, Transform parent = null)
+        {
+            var instance = Instantiate(this, pos, rot, parent);
+            instance.Memo = memo;
+            return instance;
+        }
+    }
+}
