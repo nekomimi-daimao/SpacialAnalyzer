fb9c1750b13552908a05ad0c67935c3c311957be
nekomimi-daimao <35223467+nekomimi-daimao@users.noreply.github.com>
2021-12-22 02:05:13 +0900

    activate entrypoint

--- Assets/SpacialAnalyzer/Scripts/DI/EntryPoint.cs
+++ Assets/SpacialAnalyzer/Scripts/DI/EntryPoint.cs
@@ -1,14 +1,15 @@
+using SpacialAnalyzer.Scripts.Analyzer;
 using UnityEngine;
 using VContainer;
 
-namespace SpacialAnalyzer.Scripts.Analyzer
+namespace SpacialAnalyzer.Scripts.DI
 {
     public sealed class EntryPoint : MonoBehaviour
     {
         [Inject]
         public void Initialize(AnalyzeSpatial spatial)
         {
-            // spatial.Init();
+            spatial.Init();
         }
     }
 }
